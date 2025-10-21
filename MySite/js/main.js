07eafa456247727ba91938497cc663d79e165495
Samat <118238847+SamaTjld@users.noreply.github.com>
2025-10-21 21:53:15 +0200

    Add files via upload

---
+++ MySite/js/main.js
@@ -0,0 +1,8 @@
+new WOW().init();
+const swiper = new Swiper(".swiper", {
+  pagination: {
+    el: ".projects-pagination",
+    bulletClass: ".projects-bullet",
+    bulletActiveClass: ".projects-bullet-active",
+  },
+});
